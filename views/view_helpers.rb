88642bfbcf3064974549e6f12dbc98b375af4021
Kathryn Bennett <kbennett13@college.harvard.edu>
2012-03-21 15:45:52 -0400

    Implement collection detail stub.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -119,15 +119,29 @@ NOTES
     end
   end
 
+  def load_example_collection!
+    @object = create_collection!
+  end
+
   def create_collection!
     @coll_number ||= 0
     @coll_number += 1
     OpenStruct.new.tap do |o|
       o.title = "Collection Title #{@coll_number}"
       o.created = "1786-2010 [1201 AH - 1388 SH]"
+      o.updated = "1786-2010 [1201 AH - 1388 SH]"
+      o.accession_number = "1228"
       o.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud"
+      o.acquisition_notes = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      o.restrictions = "No restrictions"
+      o.genres = ["Letters", "Objects"]
+      o.subjects = ["clothing and dress", "marriage"]
+      o.dates = "1-January-2000"
       o.url = "#"
-
+      o.history = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      o.creator = "Corleone Family"
+      o.favorites = []
+      6.times.with_object(o.favorites) { |_, acc| acc << create_item! }
       o.count = rand(125)
 
       if rand(2) == 1
@@ -136,4 +150,10 @@ NOTES
     end
   end
   
+  def create_item!
+    OpenStruct.new.tap do |o|
+      o.title = "an item"
+      o.thumbnail = "http://d19ob2c2hogwg9.cloudfront.net/thumbs/it_#{rand(2000)+1}.jpg"
+    end
+  end
 end
